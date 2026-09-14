8acf4e76371f4220738eac80e20add42785e06a5
gungun88 <pengqibao3@gmail.com>
2026-09-14 22:37:15 +0800

    fix: use Flarum 2 admin registry

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('nodeloc-ignore-hidden-tags', () => {
-  app.extensionData.for('nodeloc-ignore-hidden-tags').registerPermission(
+  app.registry.for('nodeloc-ignore-hidden-tags').registerPermission(
     {
       icon: 'fas fa-eye',
       label: app.translator.trans('ignore-hidden-tags.admin.permissions.select-group-ignore'),
